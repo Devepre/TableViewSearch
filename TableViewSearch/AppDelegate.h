7df3bd770e119947a598db97c0638276e4e2f117
Devepre <bicycle2life@gmail.com>
2018-04-05 12:50:39 +0300

    Initial Commit

---
+++ TableViewSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewSearch
+//
+//  Created by User on 4/5/18.
+//  Copyright © 2018 DEL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
